487012aa0b8d783fe405e0c74367af6891a9c3b4
--get <zhaoying@cambricon.com>
2020-04-29 12:16:28 +0000

    (feat): add box coder op, and unit test ok

---
+++ lite/kernels/mlu/bridges/box_coder_op.cc
@@ -0,0 +1,157 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lite/kernels/mlu/bridges/graph.h"
+#include "lite/kernels/mlu/bridges/utility.h"
+#include "lite/kernels/npu/bridges/registry.h"
+
+namespace paddle {
+namespace lite {
+namespace subgraph {
+namespace mlu {
+
+inline BoxCodeType GetBoxCodeType(const std::string& type) {
+  if (type == "encode_center_size") {
+    return BoxCodeType::kEncodeCenterSize;
+  } else if (type == "decode_center_size") {
+    return BoxCodeType::kDecodeCenterSize;
+  } else {
+    CHECK(false) << "ERROR type,expect one of encode_center_size &  "
+                    "decode_center_size, but got %s\n";
+  }
+}
+
+int BoxCoderConverter(void* ctx, OpLite* op, KernelBase* kernel) {
+  CHECK(ctx != nullptr);
+  CHECK(op != nullptr);
+  auto graph = static_cast<Graph*>(ctx);
+  auto op_info = op->op_info();
+  auto op_type = op_info->Type();
+  auto scope = op->scope();
+  VLOG(3) << "[MLU] Converting " + op_type + "...";
+
+  auto Prior_box_name = op_info->Input("PriorBox").front();
+  auto Target_box_name = op_info->Input("TargetBox").front();
+  auto Output_box_name = op_info->Output("OutputBox").front();
+  std::vector<std::string> input_arg_names = op_info->InputArgumentNames();
+  if (std::find(input_arg_names.begin(),
+                input_arg_names.end(),
+                "PriorBoxVar") == input_arg_names.end()) {
+    LOG(FATAL) << "box coder mlu kernel expect PriorBoxVar input" << std::endl;
+  }
+  auto box_var_name = op_info->Input("PriorBoxVar").front();
+
+  auto* prior_box = scope->FindVar(Prior_box_name)->GetMutable<Tensor>();
+  auto* target_box = scope->FindVar(Target_box_name)->GetMutable<Tensor>();
+  auto* proposals = scope->FindVar(Output_box_name)->GetMutable<Tensor>();
+  auto* box_var = scope->FindVar(box_var_name)->GetMutable<Tensor>();
+
+  auto code_type_str = op_info->GetAttr<std::string>("code_type");
+  auto box_normalized = op_info->GetAttr<bool>("box_normalized");
+  int axis = -1;
+  if (op_info->HasAttr("axis")) {
+    axis = op_info->GetAttr<int>("axis");
+  } else {
+    LOG(FATAL) << "box coder mlu kernel expect axis" << std::endl;
+  }
+
+  if (op_info->HasAttr("variance")) {
+    LOG(FATAL) << "box coder mlu kernel expect not have variance attr"
+               << std::endl;
+  }
+  BoxCodeType code_type = GetBoxCodeType(code_type_str);
+
+  int row = -1;
+  int len = -1;
+  int col = -1;
+  if (code_type == BoxCodeType::kEncodeCenterSize) {
+    // target_box_shape = {row, len};
+    // prior_box_shape = {col, len};
+    // output_shape = {row, col, len};
+    row = target_box->dims()[0];
+    len = target_box->dims()[1];
+    col = prior_box->dims()[0];
+  } else if (code_type == BoxCodeType::kDecodeCenterSize) {
+    // target_box_shape = {row,col,len};
+    // prior_box_shape = {col, len} if axis == 0, or {row, len};
+    // output_shape = {row, col, len};
+    row = target_box->dims()[0];
+    col = target_box->dims()[1];
+    len = target_box->dims()[2];
+    if (axis == 0) {
+      CHECK(prior_box->dims()[0] == col);
+    } else {
+      CHECK(prior_box->dims()[0] == row);
+    }
+  }
+
+  bool float32_precision = false;
+  if (graph->FPType() == CNML_DATA_FLOAT32) {
+    float32_precision = true;
+  }
+
+  // =================== DEBUG ======================
+  VLOG(6) << "prior_box->dims(): " << prior_box->dims();
+  VLOG(6) << "target_box->dims(): " << target_box->dims();
+  VLOG(6) << "box_var->dims(): " << box_var->dims();
+  VLOG(6) << "proposals->dims(): " << proposals->dims();
+  VLOG(6) << "code_type_str: " << code_type_str;
+  VLOG(6) << "col: " << col;
+  VLOG(6) << "row: " << row;
+  VLOG(6) << "len: " << len;
+  VLOG(6) << "axis: " << axis;
+  VLOG(6) << "box_normalized :" << box_normalized;
+  VLOG(6) << "float32_precision: " << float32_precision;
+  VLOG(6) << "Prior_box_name: " << Prior_box_name;
+  VLOG(6) << "Target_box_name: " << Target_box_name;
+  VLOG(6) << "Output_box_name: " << Output_box_name;
+  VLOG(6) << "box_var_name: " << box_var_name;
+
+  // =================== DEBUG END ======================
+  auto target_box_tensor = graph->GetNode(Target_box_name);
+  auto prior_box_tensor = graph->GetNode(Prior_box_name);
+  auto box_var_tensor = graph->GetNode(box_var_name);
+  auto proposals_tensor = graph->AddNode(Output_box_name,
+                                         proposals->dims().Vectorize(),
+                                         CNML_TENSOR,
+                                         CNML_NCHW,
+                                         graph->FPType());
+
+  cnmlBaseOp_t box_coder_op;
+  CNML_CALL(cnmlCreatePluginBoxCoderOp(&box_coder_op,
+                                       target_box_tensor->mlu_tensor(),
+                                       prior_box_tensor->mlu_tensor(),
+                                       box_var_tensor->mlu_tensor(),
+                                       proposals_tensor->mlu_tensor(),
+                                       row,
+                                       col,
+                                       len,
+                                       axis,
+                                       box_normalized,
+                                       float32_precision,
+                                       code_type));
+  // CNML_CALL(cnmlSetOperationComputingLayout(box_coder_op, CNML_NCHW)); //
+  // important
+  graph->FuseOp(box_coder_op);
+  return SUCCESS;
+}
+
+}  // namespace mlu
+}  // namespace subgraph
+}  // namespace lite
+}  // namespace paddle
+
+REGISTER_SUBGRAPH_BRIDGE(box_coder,
+                         kMLU,
+                         paddle::lite::subgraph::mlu::BoxCoderConverter);
